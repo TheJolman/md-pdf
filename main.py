a83bbdc066e04790287b864b4720ca7037725d77
josh <joshuaholman5@gmail.com>
2025-05-19 10:17:48 -0700

    start argparse

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
+import argparse
 import markdown
 from weasyprint import HTML
 
+
 def convert_markdown_to_pdf(input_file, output_file):
-    with open(input_file, 'r') as f:
+    with open(input_file, "r") as f:
         markdown_content = f.read()
 
     html_content = markdown.markdown(markdown_content)
@@ -22,33 +24,46 @@ def convert_markdown_to_pdf(input_file, output_file):
 
     HTML(string=full_html).write_pdf(output_file)
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default=None,
+        help="Specify the output path of the PDF file.",
+    )
+    args = parser.parse_args()
+    if args.output:
+        print("output flag used")
+
+
 if __name__ == "__main__":
-    argc = len(sys.argv)
-    if argc not in [2, 3]:
-        print("Usage: ./compile input.md")
-        print("Or:    ./compile input.md output.pdf")
-        sys.exit(1)
-
-    input_file = sys.argv[1]
-
-    if len(input_file) < 4 or not input_file.endswith('.md'):
-        print("Invalid markdown file name")
-        sys.exit(1)
-
-    if argc == 3:
-        output_file = sys.argv[2]
-        if len(output_file) < 5 or not output_file.endswith('.pdf'):
-            print("Invalid pdf file name")
-            sys.exit(1)
-    else:
-        output_file = f"{input_file[:-2]}pdf"
-
-    try:
-        convert_markdown_to_pdf(input_file, output_file)
-        print(f"Converted {input_file} to {output_file}")
-    except Exception as e:
-        print(f"Error converting file: {str(e)}")
-        sys.exit(1)
-
-
-
+    main()
+    # argc = len(sys.argv)
+    # if argc not in [2, 3]:
+    #     print("Usage: ./compile input.md")
+    #     print("Or:    ./compile input.md output.pdf")
+    #     sys.exit(1)
+    #
+    # input_file = sys.argv[1]
+    #
+    # if len(input_file) < 4 or not input_file.endswith('.md'):
+    #     print("Invalid markdown file name")
+    #     sys.exit(1)
+    #
+    # if argc == 3:
+    #     output_file = sys.argv[2]
+    #     if len(output_file) < 5 or not output_file.endswith('.pdf'):
+    #         print("Invalid pdf file name")
+    #         sys.exit(1)
+    # else:
+    #     output_file = f"{input_file[:-2]}pdf"
+    #
+    # try:
+    #     convert_markdown_to_pdf(input_file, output_file)
+    #     print(f"Converted {input_file} to {output_file}")
+    # except Exception as e:
+    #     print(f"Error converting file: {str(e)}")
+    #     sys.exit(1)
